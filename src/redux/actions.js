b8e9a99b43f62dfd0f23eb5b08a1e96a33c50953
ABogunets <abogunets@gmail.com>
2023-04-21 00:16:48 +0300

    done

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,30 +1,5 @@
 import { nanoid } from 'nanoid';
 
-// const checkName = name => {
-
-//   const normalizedName = name.toLowerCase();
-//   const foundName = contacts.find(
-//     contact => contact.name.toLowerCase() === normalizedName
-//   );
-//   if (foundName) {
-//     alert(`${name} is already in contacts.`);
-//     return true;
-//   }
-// };
-
-// export const addContact = (name, number) => {
-//   if (!checkName(name)) {
-//     return {
-//       type: 'tasks/addContact',
-//       payload: {
-//         id: nanoid(),
-//         name,
-//         number,
-//       },
-//     };
-//   }
-// };
-
 export const addContact = (name, number) => {
   return {
     type: 'contacts/addContact',
